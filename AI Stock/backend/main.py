8f613b415f4853239c5bd567becbc9f4d9d88fbc
Aryan <aryanassit@gmail.com>
2026-04-06 09:37:13 +0530

    Update main.py

--- AI Stock/backend/main.py
+++ AI Stock/backend/main.py
@@ -48,7 +48,7 @@ def verify_password(plain_password, hashed_password):
 def get_password_hash(password):
     return pwd_context.hash(password)
 
-NEWS_API_KEY = "" # you live news API from NEWS API org. 
+NEWS_API_KEY = "" # you live news API from NEWS API org.
 
 print("Loading FinBERT Model...")
 sentiment_pipeline = pipeline("sentiment-analysis", model="ProsusAI/finbert")
